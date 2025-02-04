f391daee1a9632c3e56408b70a250361543c2a37
forest-6 <seungmin3238@gmail.com>
2025-02-04 15:53:34 +0900

    Feat: 비밀번호 검증 로직 추가
    9자리 이상 &&
    숫자, 문자, 특수문자를 혼합 &&
    동일문자 또는 숫자 3회 이상 반복 금지  &&
    연속하여 오름차순 또는 내림차순이 아닐 것 &&
    ID와 동일하지 않을 것

--- src/views/ZodView.vue
+++ src/views/ZodView.vue
@@ -5,15 +5,22 @@
       <span>
         <label for="id">아이디</label>
         <div class="input-wrapper">
-          <input type="text" id="id" v-model="id" />
+          <input type="text" id="id" v-model="idInput" />
           <span class="input-valid-message">{{ idValidMessage }}</span>
         </div>
       </span>
       <span>
         <label for="pw">비밀번호</label>
         <div class="input-wrapper">
-          <input type="text" id="pw" v-model="pw" />
+          <input type="text" id="pw" v-model="pwInput" />
           <span class="input-valid-message">{{ pwValidMessage }}</span>
+        </div>
+      </span>
+      <span>
+        <label for="rePw">비밀번호 재확인</label>
+        <div class="input-wrapper">
+          <input type="text" id="rePw" v-model="rePwInput" />
+          <span class="input-valid-message">{{ rePwValidMessage }}</span>
         </div>
       </span>
       <button @click="createUser">사용자 추가</button>
@@ -27,44 +34,73 @@ import { z } from 'zod'
 
 import { createUserApi } from '@/apis/users'
 
-const id = ref('')
-const pw = ref('')
+const idInput = ref('')
+const pwInput = ref('')
+const rePwInput = ref('')
+
 const idValidMessage = ref('')
 const pwValidMessage = ref('')
+const rePwValidMessage = ref('')
 
-const UserId = z.string().min(4, '아이디는 4글자 이상이어야 합니다.')
-const UserPw = z.string().min(4, '비밀번호는 4글자 이상이어야 합니다.')
+const JoinId = z.string().min(1, '글자를 입력해주세요.')
+
+const JoinPw = z
+  .string()
+  .min(9, '9글자 이상이어야 합니다.')
+  .refine((value) => /[a-zA-Z]/.test(value), '영문자가 포함되어야 합니다.')
+  .refine((value) => /\d/.test(value), '숫자가 포함되어야 합니다.')
+  .refine((value) => /[^\w\s]/.test(value), '특수 문자가 포함되어야 합니다.')
+  .refine((value) => !/(.)\1{2,}/.test(value), '문자나 숫자가 3회 이상 반복될 수 없습니다.')
+  .refine((value) => {
+    const minLength = 3
+    const parts = value
+      .split(/([a-zA-Z]+|\d+)/) // 숫자,영문자 구분 및 그외 문자 구분
+      .map((part) => part.replace(/[^a-zA-Z0-9]/g, '')) // 숫자,영문자 외 문자 제거
+      .filter(Boolean)
+
+    for (const part of parts) {
+      if (part.length >= minLength && isSequential(part)) {
+        return false // 오름차순 또는 내림차순이면 false
+      }
+    }
+    return true // 3글자 이상 연속된 오름차순 또는 내림차순이 없으면 true
+  }, '문자나 숫자가 연속된 오름차순 또는 내림차순일 수 없습니다.')
+  .refine((value) => idInput.value !== value, '아이디와 비밀번호는 같을 수 없습니다.')
 
 const idValid = () => {
   try {
-    UserId.parse(id.value)
+    JoinId.parse(idInput.value)
     idValidMessage.value = ''
   } catch (e) {
     if (e instanceof z.ZodError) {
-      e.errors.forEach((error) => {
-        idValidMessage.value = error.message
-        console.log(`${error.path[0]}: ${error.message}`)
-      })
+      idValidMessage.value = e.errors[0].message
     }
   }
 }
 
 const pwValid = () => {
   try {
-    UserPw.parse(pw.value)
+    JoinPw.parse(pwInput.value)
     pwValidMessage.value = ''
   } catch (e) {
     if (e instanceof z.ZodError) {
-      e.errors.forEach((error) => {
-        pwValidMessage.value = error.message
-        console.log(`${error.path[0]}: ${error.message}`)
-      })
+      pwValidMessage.value = e.errors[0].message
     }
   }
 }
 
-watch([id], () => idValid())
-watch([pw], () => pwValid())
+const rePwValid = () => {
+  if (pwInput.value === rePwInput.value) {
+    rePwValidMessage.value = ''
+  } else {
+    rePwValidMessage.value = '비밀번호가 일치하지 않습니다.'
+  }
+}
+
+watch([idInput], () => idValid())
+watch([pwInput], () => pwValid())
+watch([idInput, pwInput], () => pwValid())
+watch([pwInput, rePwInput], () => rePwValid())
 
 //TODO: 사용자 추가 form 추가
 const createUser = async () => {
@@ -74,6 +110,37 @@ const createUser = async () => {
     email: 'aaaaa',
     age: 5,
   })
+}
+
+const isSequential = (str: string) => {
+  let increasing = false
+  let decreasing = false
+  let increasingCount = 0
+  let decreasingCount = 0
+
+  // 오름차순 확인
+  for (let i = 0; i < str.length; i++) {
+    if (str[i + 1] === String.fromCharCode(str.charCodeAt(i) + 1)) {
+      increasingCount++
+    } else increasingCount = 0
+    if (increasingCount >= 2) {
+      increasing = true
+      break
+    }
+  }
+  // 내림차순 확인
+  for (let i = 0; i < str.length; i++) {
+    if (str[i + 1] === String.fromCharCode(str.charCodeAt(i) - 1)) {
+      decreasingCount++
+    } else decreasingCount = 0
+    if (decreasingCount >= 2) {
+      decreasing = true
+      break
+    }
+  }
+
+  if (increasing || decreasing) return true
+  return false
 }
 </script>
 
@@ -100,11 +167,11 @@ const createUser = async () => {
 .input-wrapper {
   display: flex;
   flex-direction: column;
-  margin-bottom: 10px;
 }
 
 button {
   height: 30px;
+  margin-top: 10px;
   background-color: gainsboro;
   border: 1px solid gainsboro;
   border-radius: 10px;
