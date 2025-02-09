44c56deb6f82d4f01469d9971d0f998341f561a8
seungmin lim <seungmin3238@gmail.com>
2025-02-09 15:28:52 +0900

    Feat: 사용자 생성 기능 추가

--- src/views/TanstackView.vue
+++ src/views/TanstackView.vue
@@ -1,5 +1,27 @@
 <template>
   <main>
+    <h1>사용자 추가</h1>
+    <div class="user-add-form">
+      <span>
+        <label for="name">이름</label>
+        <div class="input-wrapper">
+          <input v-model="newUser.name" type="text" id="name" />
+        </div>
+      </span>
+      <span>
+        <label for="email">이메일</label>
+        <div class="input-wrapper">
+          <input v-model="newUser.email" type="email" id="email" />
+        </div>
+      </span>
+      <span>
+        <label for="age">나이</label>
+        <div class="input-wrapper">
+          <input v-model="newUser.age" type="text" id="age" />
+        </div>
+      </span>
+      <button class="user-add-btn" :disabled="!userInputValid" @click="addUser">추가</button>
+    </div>
     <div>
       <h1>사용자 목록</h1>
       <p v-if="isLoading">Loading...</p>
@@ -15,7 +37,7 @@
           </thead>
           <tbody>
             <tr v-for="user in users" :key="user.id">
-              <td>{{ user.firstName }} {{ user.lastName }}</td>
+              <td>{{ user.name }}</td>
               <td>{{ user.email }}</td>
               <td>{{ user.age }}</td>
             </tr>
@@ -28,8 +50,17 @@
 </template>
 
 <script setup lang="ts">
-import { getUsersApi } from '@/apis/users'
-import { useQuery } from '@tanstack/vue-query'
+import { getUsersApi, createUserApi } from '@/apis/users'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/vue-query'
+import { computed, ref } from 'vue'
+import type { User } from '@/types/user'
+
+const queryClient = useQueryClient()
+const newUser = ref({} as User)
+
+const userInputValid = computed(() => {
+  return newUser.value.name && newUser.value.email && newUser.value.age
+})
 
 const {
   data: users,
@@ -43,9 +74,62 @@ const {
   gcTime: 10000, // 10초 후 캐시 삭제
   // refetchInterval: 8000, // 8초 후 자동 새로고침
 })
+
+const mutation = useMutation({
+  mutationFn: createUserApi,
+  onSuccess: (data) => {
+    console.log('✅ 게시글 추가 성공:', data)
+    queryClient.invalidateQueries({ queryKey: ['users'] })
+    newUser.value = {} as User
+  },
+  onError: (error) => {
+    console.error('❌ 게시글 추가 실패:', error)
+  },
+})
+
+const addUser = () => {
+  mutation.mutate(newUser.value)
+}
 </script>
 
 <style lang="scss" scoped>
+.user-add-form {
+  border: 1px solid black;
+  border-radius: 10px;
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.input-wrapper {
+  width: 250px;
+  display: flex;
+  flex-direction: column;
+  input {
+    height: 30px;
+    margin-bottom: 5px;
+  }
+}
+
+button {
+  height: 30px;
+  margin-top: 10px;
+  border-radius: 10px;
+  font-weight: bold;
+  cursor: pointer;
+}
+.user-add-btn {
+  background: #2ca02c;
+  border: 1px solid #2ca02c;
+  color: #fff;
+  &:disabled {
+    background: lightgray;
+    border: 1px solid lightgray;
+    cursor: not-allowed;
+  }
+}
+
 table {
   width: 100%;
   border-collapse: collapse;
@@ -58,10 +142,6 @@ td {
 }
 
 th {
-  background-color: #f4f4f4;
-}
-
-tbody tr:nth-child(odd) {
-  background-color: #f9f9f9;
+  background-color: #cacaca;
 }
 </style>
